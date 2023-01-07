25e3fbc4e57d84205349784425a08b2bc56ffb09
safwan zullash <wi21b030@technikum-wien.at>
2023-01-07 20:34:43 +0100

    extracting style tags to css-sheet

--- inc/teamsection.php
+++ inc/teamsection.php
@@ -13,7 +13,7 @@
     <div class="row text-center">
         <!-- Team item-->
         <div class="col-xl-6 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="./uploads/team/Lichtbild.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="./uploads/team/Lichtbild.jpg" alt="" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm team-size">
                 <h5 class="mb-0">Safwan Zullash</h5><span class="small text-uppercase text-muted">Leiter</span>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
@@ -29,7 +29,7 @@
 
         <!-- Team item-->
         <div class="col-xl-6 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="./uploads/team/bild.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="./uploads/team/bild.jpg" alt="" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm team-size">
                 <h5 class="mb-0">Mohammed Laktaoui</h5><span class="small text-uppercase text-muted">Co-Leiter</span>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
